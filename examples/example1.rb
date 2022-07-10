e0ba1f3058a76deaefbfc6ab26cc3668ceff418a
Ivo Anjo <ivo@ivoanjo.me>
2022-07-10 17:28:18 +0100

    Make example1 more interesting

--- examples/example1.rb
+++ examples/example1.rb
@@ -1,5 +1,18 @@
 require "gvl-tracing"
 
+def fib(n)
+  return n if n <= 1
+  fib(n - 1) + fib(n - 2)
+end
+
 GvlTracing.start("example1.json")
+
+Thread.new { sleep(0.05) while true }
+
+sleep(0.05)
+
+3.times.map { Thread.new { fib(37) } }.map(&:join)
+
+sleep(0.05)
 
 GvlTracing.stop
