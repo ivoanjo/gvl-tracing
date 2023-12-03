23ce67bca027a73cfa78dea238ea705323d87ed1
JP Camara <jp@jpcamara.com>
2023-12-03 13:33:47 -0500

    Fixes block form of #start
    * Because the ensure calls `_stop` instead of `stop`, the block form produces invalid json
    
    * Perfetto is pretty permissive about this and still accepts the file - but other tools break and JSON parsing is broken
    
    * Also adds first specs for the project, covering a few different scenarios 🎉

--- gems.rb
+++ gems.rb
@@ -7,5 +7,7 @@ gemspec
 gem "rake", "~> 13.0"
 gem "rake-compiler", "~> 1.2"
 gem "pry"
+gem "pry-byebug"
+gem "rspec"
 gem "standard", "~> 1.12"
 gem "concurrent-ruby"
---
+++ spec/perfetto_trace.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class PerfettoTrace
+  def initialize(file_path)
+    @trace = JSON.parse(File.read(file_path))
+  end
+
+  def threads
+    @trace
+      .select { |j| j["ph"] == "M" && j["name"] == "thread_name" }
+      .uniq { |j| j["args"]["tid"] }
+      .map { |j| Row.new(j) }
+  end
+
+  def non_main_threads
+    threads.select { |t| t.thread_name != "Main Thread" }
+  end
+
+  def events_by_thread
+    @trace
+      .group_by { |j| j["tid"] }
+      .map { |tid, events| [tid, events.map { |j| Row.new(j) }] }
+      .to_h
+  end
+
+  class Row
+    def initialize(row)
+      @row = row
+    end
+
+    def meta? = ph == "M"
+
+    def phase_begin? = ph == "B"
+
+    def phase_end? = ph == "E"
+
+    def phase = @row["ph"]
+
+    def pid = @row["pid"]
+
+    def tid = @row["tid"]
+
+    def ts = @row["ts"]
+
+    def name = @row["name"]
+
+    def args = @row["args"]
+
+    def thread_name = @row.dig("args", "name")
+  end
+end
