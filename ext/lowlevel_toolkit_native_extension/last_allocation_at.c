5e4caca9902da702ca881161fa3f902af004c914
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-04-05 00:48:34 +0100

    Make stuff static again

--- ext/lowlevel_toolkit_native_extension/last_allocation_at.c
+++ ext/lowlevel_toolkit_native_extension/last_allocation_at.c
@@ -6,7 +6,7 @@ int rb_objspace_internal_object_p(VALUE obj);
 #define MAX_DEPTH 1000
 static VALUE stack = Qnil;
 
-void on_newobj_event(VALUE tpval, RB_UNUSED_VAR(void *_)) {
+static void on_newobj_event(VALUE tpval, RB_UNUSED_VAR(void *_)) {
   if (stack == Qnil ||
     rb_objspace_internal_object_p(rb_tracearg_object(rb_tracearg_from_tracepoint(tpval)))) return;
   VALUE buffer[MAX_DEPTH];
@@ -15,14 +15,14 @@ void on_newobj_event(VALUE tpval, RB_UNUSED_VAR(void *_)) {
   for (int i = 0; i < depth; i++) rb_ary_push(stack, buffer[i]);
 }
 
-VALUE track_last_allocation_at(RB_UNUSED_VAR(VALUE _)) {
+static VALUE track_last_allocation_at(RB_UNUSED_VAR(VALUE _)) {
   stack = rb_ary_new_capa(MAX_DEPTH);
   VALUE tp = rb_tracepoint_new(0, RUBY_INTERNAL_EVENT_NEWOBJ, on_newobj_event, NULL);
   rb_tracepoint_enable(tp); rb_yield(Qnil); rb_tracepoint_disable(tp);
   return Qnil;
 }
 
-VALUE last_allocation_at(RB_UNUSED_VAR(VALUE _)) {
+static VALUE last_allocation_at(RB_UNUSED_VAR(VALUE _)) {
   VALUE raw_stack = stack;
   stack = Qnil; // Pause recording of stacks while we're doing the copying below
   VALUE result = rb_ary_new();
--- ext/lowlevel_toolkit_native_extension/who_called_me.c
+++ ext/lowlevel_toolkit_native_extension/who_called_me.c
@@ -1,15 +1,15 @@
 #include <ruby/ruby.h>
 #include <ruby/debug.h>
 
-VALUE callback(const rb_debug_inspector_t *dbg_context, void *data) {
+static VALUE callback(const rb_debug_inspector_t *dbg_context, void *data) {
   bool return_self = (data != NULL);
   return return_self ?
     // Depth: 0 -> us; 1 -> caller; 2 -> caller of caller
     rb_debug_inspector_frame_self_get(dbg_context, 2) : rb_debug_inspector_frame_binding_get(dbg_context, 2);
 }
 
-VALUE who_called_me(VALUE self) { return rb_debug_inspector_open(callback, (void *) !NULL); }
-VALUE who_called_me_binding(VALUE self) { return rb_debug_inspector_open(callback, NULL); }
+static VALUE who_called_me(VALUE self) { return rb_debug_inspector_open(callback, (void *) !NULL); }
+static VALUE who_called_me_binding(VALUE self) { return rb_debug_inspector_open(callback, NULL); }
 
 void init_who_called_me(VALUE lowlevel_toolkit_module) {
   rb_define_singleton_method(lowlevel_toolkit_module, "who_called_me", who_called_me, 0);
