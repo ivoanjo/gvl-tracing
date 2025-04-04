c99399ce200b4456c7c4e0263f5f8e68fe734835
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-04-05 00:22:22 +0100

    Tiny cleanup

--- ext/lowlevel_toolkit_native_extension/who_called_me.c
+++ ext/lowlevel_toolkit_native_extension/who_called_me.c
@@ -1,17 +1,17 @@
 #include <ruby/ruby.h>
 #include <ruby/debug.h>
 
-static VALUE callback(const rb_debug_inspector_t *dbg_context, void *data) {
+VALUE callback(const rb_debug_inspector_t *dbg_context, void *data) {
   bool return_self = (data != NULL);
   return return_self ?
     // Depth: 0 -> us; 1 -> caller; 2 -> caller of caller
     rb_debug_inspector_frame_self_get(dbg_context, 2) : rb_debug_inspector_frame_binding_get(dbg_context, 2);
 }
 
-void init_who_called_me(VALUE lowlevel_toolkit_module) {
-  VALUE who_called_me(VALUE self) { return rb_debug_inspector_open(callback, (void *) !NULL); }
-  VALUE who_called_me_binding(VALUE self) { return rb_debug_inspector_open(callback, NULL); }
+VALUE who_called_me(VALUE self) { return rb_debug_inspector_open(callback, (void *) !NULL); }
+VALUE who_called_me_binding(VALUE self) { return rb_debug_inspector_open(callback, NULL); }
 
+void init_who_called_me(VALUE lowlevel_toolkit_module) {
   rb_define_singleton_method(lowlevel_toolkit_module, "who_called_me", who_called_me, 0);
   rb_define_singleton_method(lowlevel_toolkit_module, "who_called_me_binding", who_called_me_binding, 0);
 }
