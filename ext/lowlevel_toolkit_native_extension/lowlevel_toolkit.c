e111a1581c13d1939445df8985bffcf4e9007fc7
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-04-05 01:02:55 +0100

    Extract `track_objects_created`

--- ext/lowlevel_toolkit_native_extension/lowlevel_toolkit.c
+++ ext/lowlevel_toolkit_native_extension/lowlevel_toolkit.c
@@ -25,20 +25,8 @@
 
 #include <ruby/ruby.h>
 #include <ruby/debug.h>
-#include <time.h>
-#include "extconf.h"
 
-int rb_objspace_internal_object_p(VALUE obj);
-
-#ifdef __GNUC__
-  #define UNUSED_ARG  __attribute__((unused))
-#else
-  #define UNUSED_ARG
-#endif
-
-static void on_newobj_event(VALUE tpval, void *data);
-static VALUE track_objects_created(VALUE self);
-
+void init_track_objects_created(VALUE lowlevel_toolkit_module);
 void init_print_gc_timing(VALUE lowlevel_toolkit_module);
 void init_on_gc_finish(VALUE lowlevel_toolkit_module);
 void init_who_called_me(VALUE lowlevel_toolkit_module);
@@ -47,26 +35,9 @@ void init_last_allocation_at(VALUE lowlevel_toolkit_module);
 void Init_lowlevel_toolkit_native_extension(void) {
   VALUE lowlevel_toolkit_module = rb_define_module("LowlevelToolkit");
 
-  rb_define_singleton_method(lowlevel_toolkit_module, "track_objects_created", track_objects_created, 0);
+  init_track_objects_created(lowlevel_toolkit_module);
   init_print_gc_timing(lowlevel_toolkit_module);
   init_on_gc_finish(lowlevel_toolkit_module);
   init_last_allocation_at(lowlevel_toolkit_module);
   init_who_called_me(lowlevel_toolkit_module);
-}
-
-static VALUE track_objects_created(VALUE self) {
-  VALUE result = rb_ary_new();
-  VALUE tp = rb_tracepoint_new(0, RUBY_INTERNAL_EVENT_NEWOBJ, on_newobj_event, (void *) result);
-  rb_tracepoint_enable(tp);
-  rb_yield(Qnil);
-  rb_tracepoint_disable(tp);
-  // Filter out any objects that were marked as hidden after being created
-  for (int i = 0; i < RARRAY_LEN(result); i++) if (RBASIC_CLASS(rb_ary_entry(result, i)) == 0) rb_ary_store(result, i, Qnil);
-  return result;
-}
-
-static void on_newobj_event(VALUE tpval, void *data) {
-  VALUE result = (VALUE) data;
-  VALUE obj = rb_tracearg_object(rb_tracearg_from_tracepoint(tpval));
-  if (!rb_objspace_internal_object_p(obj)) rb_ary_push(result, obj);
 }
---
+++ ext/lowlevel_toolkit_native_extension/track_objects_created.c
@@ -0,0 +1,26 @@
+#include <ruby/ruby.h>
+#include <ruby/debug.h>
+
+int rb_objspace_internal_object_p(VALUE obj);
+
+static void on_newobj_event(VALUE tpval, void *data) {
+  VALUE obj = rb_tracearg_object(rb_tracearg_from_tracepoint(tpval));
+  if (!rb_objspace_internal_object_p(obj)) rb_ary_push((VALUE) data, obj);
+}
+
+static VALUE filter_hidden_objects(VALUE result) {
+  for (int i = 0; i < RARRAY_LEN(result); i++)
+    if (!RBASIC_CLASS(rb_ary_entry(result, i))) rb_ary_store(result, i, Qnil);
+  return result;
+}
+
+static VALUE track_objects_created(VALUE self) {
+  VALUE result = rb_ary_new();
+  VALUE tp = rb_tracepoint_new(0, RUBY_INTERNAL_EVENT_NEWOBJ, on_newobj_event, (void *) result);
+  rb_tracepoint_enable(tp); rb_yield(Qnil); rb_tracepoint_disable(tp);
+  return filter_hidden_objects(result);
+}
+
+void init_track_objects_created(VALUE lowlevel_toolkit_module) {
+  rb_define_singleton_method(lowlevel_toolkit_module, "track_objects_created", track_objects_created, 0);
+}
